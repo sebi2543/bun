18cceef88b3b6010ed827fdae117fe906240657c
Eusebiu <Eusebiu>
2022-07-27 20:41:35 +0300

    lombok

---
+++ src/main/java/com/example/demo/model/model.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class model {
+    public String firstname;
+    public String lastName;
+    public int age;
+
+
+
+}
