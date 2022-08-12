2afe03fd867d132f91cb335de191e6db60d02634
Eusebiu <Eusebiu>
2022-08-12 18:29:37 +0300

    initial commit

--- src/main/java/com/example/demo/controller/InstructorController.java
+++ src/main/java/com/example/demo/controller/InstructorController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.InstructorDTO;
 import com.example.demo.entity.Instructor;
 import com.example.demo.exception.InvalidName;
+import com.example.demo.mapper.InstructorMapper;
 import com.example.demo.service.InstructorRepositoryService;
 import com.example.demo.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "instructor")
@@ -18,6 +24,9 @@ public class InstructorController {
     @Autowired
     InstructorRepositoryService instructorRepositoryService;
 
+    @Autowired
+    InstructorMapper instructorMapper;
+
     @GetMapping(value = {"/all"})
     public List<Instructor> showMainPage() {
         return instructorRepositoryService.showAll();
@@ -25,10 +34,11 @@ public class InstructorController {
     }
 
     @PostMapping("/add")
-    public Instructor add(@RequestBody Instructor instructor) throws InvalidName {
+    public HttpEntity<InstructorDTO> add(@RequestBody Instructor instructor) throws InvalidName {
         instructorService.checkInstructor(instructor);
         instructorRepositoryService.save(instructor);
-        return instructor;
+        InstructorDTO instructorDTO= instructorMapper.InstructorToDTO(instructor);
+        return new HttpEntity<InstructorDTO>(instructorDTO);
     }
 
     @PostMapping("/search")
--- src/main/java/com/example/demo/service/InstructorServiceImpl.java
+++ src/main/java/com/example/demo/service/InstructorServiceImpl.java
@@ -2,10 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Instructor;
 import com.example.demo.exception.InvalidName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InstructorServiceImpl implements InstructorService{
+
+    @Autowired
+    InstructorRepositoryService instructorRepositoryService;
     @Override
     public void checkInstructor(Instructor instructor) throws InvalidName {
         if (instructor.getLastName().length()==0 || instructor.getFirstName().length()==0)
