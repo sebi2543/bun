e56d38982e8a708d8c69cd968ba61d940d5b0ea3
Eusebiu <Eusebiu>
2022-08-16 13:55:11 +0300

    initial commit

--- src/main/java/com/example/demo/mapper/CourseMapper.java
+++ src/main/java/com/example/demo/mapper/CourseMapper.java
@@ -3,7 +3,6 @@ package com.example.demo.mapper;
 import com.example.demo.dto.CourseDTO;
 import com.example.demo.entity.Course;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
--- src/main/java/com/example/demo/mapper/InstructorMapper.java
+++ src/main/java/com/example/demo/mapper/InstructorMapper.java
@@ -3,7 +3,6 @@ package com.example.demo.mapper;
 import com.example.demo.dto.InstructorDTO;
 import com.example.demo.entity.Instructor;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface InstructorMapper {
